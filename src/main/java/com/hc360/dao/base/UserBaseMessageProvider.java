7e5e876f73176e29884ef381755e466c7cf53d58
冯达 <fengda@hc360.com>
2019-02-26 15:00:26 +0800

    修改sql

--- src/main/java/com/hc360/dao/base/UserBaseMessageProvider.java
+++ src/main/java/com/hc360/dao/base/UserBaseMessageProvider.java
@@ -15,97 +15,85 @@ import org.apache.ibatis.jdbc.SQL;
  */
 public class UserBaseMessageProvider {
 
-    public String SelectUserWithParam(OnCorTable onCorTable){
+    public String SelectUserWithParam(OnCorTable onCorTable) {
 
-        return new SQL(){
-            {
-                StringBuilder sql = new StringBuilder();
+        StringBuilder sql = new StringBuilder();
 
-                sql.append("select oct.providerid,oct.name,oct.address,oct.contacter,oct.telephone,oct.mp,oct.username,");
-                sql.append("oct.checked,oct.states,oct.memtypeid,oct.areaid,oct.userid,oct.unchecked,oct.freeshop ");
-                sql.append(" from on_cor_table oct where 1 = 1");
+        sql.append("select oct.providerid,oct.name,oct.address,oct.contacter,oct.telephone,oct.mp,oct.username,");
+        sql.append("oct.checked,oct.states,oct.memtypeid,oct.areaid,oct.userid,oct.unchecked,oct.freeshop ");
+        sql.append(" from on_cor_table oct where 1 = 1");
 
-                if (onCorTable.getUserid() != 0){
-                    sql.append(" and oct.userid = " + onCorTable.getUserid());
-                }
+        if (onCorTable.getUserid() != 0) {
+            sql.append(" and oct.userid = " + onCorTable.getUserid());
+        }
 
-                if(onCorTable.getProvinceid() != 0){
-                    sql.append(" and oct.providerid = " + onCorTable.getProvinceid());
-                }
+        if (onCorTable.getProvinceid() != 0) {
+            sql.append(" and oct.providerid = " + onCorTable.getProvinceid());
+        }
 
-                if(StringUtils.isNotBlank(onCorTable.getChecked())){
-                    sql.append(" and oct.checked = " + onCorTable.getChecked());
-                }
+        if (StringUtils.isNotBlank(onCorTable.getChecked())) {
+            sql.append(" and oct.checked = " + onCorTable.getChecked());
+        }
 
-                if(StringUtils.isNotBlank(onCorTable.getStates())){
-                    sql.append(" and oct.states = " + onCorTable.getStates());
-                }
+        if (StringUtils.isNotBlank(onCorTable.getStates())) {
+            sql.append(" and oct.states = " + onCorTable.getStates());
+        }
 
-                if(StringUtils.isNotBlank(onCorTable.getUnchecked())){
-                    sql.append(" and oct.unchecked = " + onCorTable.getUnchecked());
-                }
-            }
-        }.toString();
+        if (StringUtils.isNotBlank(onCorTable.getUnchecked())) {
+            sql.append(" and oct.unchecked = " + onCorTable.getUnchecked());
+        }
+
+        return sql.toString();
     }
 
-    public String findLeaveWordCount(RecvnotesParam recvparam){
-       return new SQL(){
-            {
-                StringBuilder sql = new StringBuilder();
-                sql.append("SELECT count(a.id) from BusNote a where a.recvproviderid = #{provinceid}");
-                sql.append(" and a.states='0' and (a.recvdel='0' or a.recvdel is null) AND a.isgarbage=0");
-                sql.append(" and ((a.lstate= '0' and a.notekind='0') or (a.lstate='3' and a.notekind='1'))");
-                sql.append(" and (a.sendproviderid = #{anymoussender} or exists");
-                sql.append(" (select 'x' from OnCorTable c where c.id=a.sendproviderid and c.states='0'))");
+    public String findLeaveWordCount(RecvnotesParam recvparam) {
+        StringBuilder sql = new StringBuilder();
+        sql.append("SELECT count(a.id) from BusNote a where a.recvproviderid = #{provinceid}");
+        sql.append(" and a.states='0' and (a.recvdel='0' or a.recvdel is null) AND a.isgarbage=0");
+        sql.append(" and ((a.lstate= '0' and a.notekind='0') or (a.lstate='3' and a.notekind='1'))");
+        sql.append(" and (a.sendproviderid = #{anymoussender} or exists");
+        sql.append(" (select 'x' from OnCorTable c where c.id=a.sendproviderid and c.states='0'))");
 
-                // 相关信息类型
-                if (StringUtils.isNotBlank(recvparam.getInfotype())) {
-                    sql.append( " and a.infotype = " + recvparam.getInfotype() );
-                }
-                // 只查询非系统留言
-                if ( recvparam.isNotSysnote() ) {
-                    sql.append( " and ( a.notetype =0 or a.notetype =1 ) " );
-                }
-            }
-        }.toString();
+        // 相关信息类型
+        if (StringUtils.isNotBlank(recvparam.getInfotype())) {
+            sql.append(" and a.infotype = " + recvparam.getInfotype());
+        }
+        // 只查询非系统留言
+        if (recvparam.isNotSysnote()) {
+            sql.append(" and ( a.notetype =0 or a.notetype =1 ) ");
+        }
+
+        return sql.toString();
     }
 
 
-    public String findCity(CityVo cityVo){
-        return new SQL(){
-            {
-                StringBuilder sql = new StringBuilder();
-                sql.append("SELECT * FROM CITY c where 1 = 1");
+    public String findCity(CityVo cityVo) {
+        StringBuilder sql = new StringBuilder();
+        sql.append("SELECT * FROM CITY c where 1 = 1");
 
-                if(cityVo.getId() != null){
-                    sql.append(" c.id = " + cityVo.getId());
-                }
+        if (cityVo.getId() != null) {
+            sql.append(" c.id = " + cityVo.getId());
+        }
 
-                if(StringUtils.isNotBlank(cityVo.getCityCode())){
-                    sql.append(" c.citycode = " + cityVo.getCityCode());
-                }
-            }
+        if (StringUtils.isNotBlank(cityVo.getCityCode())) {
+            sql.append(" c.citycode = " + cityVo.getCityCode());
+        }
 
-
-        }.toString();
+        return sql.toString();
     }
 
-    public String findProvince(ProvinceVo provinceVo){
-        return new SQL(){
-            {
-                StringBuilder sql = new StringBuilder();
-                sql.append("SELECT * FROM PROVINCE p where 1 = 1");
+    public String findProvince(ProvinceVo provinceVo) {
+        StringBuilder sql = new StringBuilder();
+        sql.append("SELECT * FROM PROVINCE p where 1 = 1");
 
-                if(provinceVo.getId() != null){
-                    sql.append(" p.id = " + provinceVo.getId());
-                }
+        if (provinceVo.getId() != null) {
+            sql.append(" p.id = " + provinceVo.getId());
+        }
 
-                if(null != provinceVo.getCountryId()){
-                    sql.append(" p.country_id = " + provinceVo.getCountryId());
-                }
-            }
+        if (null != provinceVo.getCountryId()) {
+            sql.append(" p.country_id = " + provinceVo.getCountryId());
+        }
 
-
-        }.toString();
+        return sql.toString();
     }
 }
