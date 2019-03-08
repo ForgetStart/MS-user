66c369ad8cdf15676d353c2536a704b3425e29b7
高兴坤 <gaoxingkun@hc360.com>
2019-03-08 16:49:57 +0800

    更改提交方式

--- src/main/java/com/hc360/controller/TempicsController.java
+++ src/main/java/com/hc360/controller/TempicsController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -46,7 +47,7 @@ public class TempicsController {
      * @throws
      * @return
      */
-    @RequestMapping(value = "/getuploadsessionid")
+    @RequestMapping(value = "/getuploadsessionid",method = RequestMethod.POST)
     public Long getUploadSessionId() throws Exception{
         return temppicsService.getUploadSessionId();
     }
